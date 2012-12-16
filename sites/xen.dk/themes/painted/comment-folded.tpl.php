3d003a8898ed89339ecb8ea80199ac931b53b37e
Thomas Fini Hansen <xen@xen.dk>
2012-12-16 19:54:48 +0100

    Removed unneeded modules/themes.

--- sites/xen.dk/themes/painted/comment-folded.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="comment comment-folded">
-  <span class="subject"><?php print $title; if (!empty($new)): ?> <span class="new"><?php echo $new; ?></span><?php endif; ?></span><span class="credit"><?php print t(' by') .' '. $author; ?></span>
-</div>
--- sites/xen.dk/themes/painted/comment-wrapper.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if (!$content || $node_type == 'forum') {
-    echo '<div id="comments">'. $content . '</div>';
-  }
-  else {
-    echo '<div id="comments"><h2 class="comments">'. t('Comments') .'</h2>'. $content .'</div>';
-  }
-?>
