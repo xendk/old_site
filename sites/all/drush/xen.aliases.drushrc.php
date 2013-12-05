17b02cdbae9942177293370c2d9165bbc946b063
Thomas Fini Hansen <xen@xen.dk>
2013-12-05 11:19:09 +0100

    Added Drush aliases.

---
+++ sites/all/drush/xen.aliases.drushrc.php
@@ -0,0 +1,24 @@
+<?php
+
+$aliases['common'] = array(
+  'uri' => 'default',
+  'remote-host' => 'xen.dk',
+  'remote-user' => 'root',
+  'path-aliases' => array(
+    '%drush' => '/root/drush',
+    '%drush-script' => '/root/drush/drush',
+  ),
+);
+
+$aliases['stg'] = array(
+  'parent' => '@xen.common',
+  'uri' => 'stg.xen.dk',
+  'root' => '/var/www/stg.xen.dk',
+  'git-branch' => 'deployment-testing',
+);
+
+$aliases['live'] = array(
+  'parent' => '@xen.common',
+  'uri' => 'xen.dk',
+  'root' => '/var/www/xen.dk',
+);
