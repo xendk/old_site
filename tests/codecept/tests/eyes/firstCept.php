110c29814b90d0f2d302ef433768202decdfd15e
Thomas Fini Hansen <xen@xen.dk>
2015-04-25 09:57:35 +0200

    Now with Argus testing.

--- tests/codecept/tests/eyes/firstCept.php
+++ tests/codecept/tests/eyes/firstCept.php
@@ -2,4 +2,11 @@
 $I = new EyesTester($scenario);
 $I->wantTo('check that the frontpage is OK');
 $I->amOnPage('/');
-$I->eyeball('the frontpage');
+$I->eyeball('frontpage');
+$I->see('Bandaid');
+
+$I->amOnPage('/en/2014/04/28/point-bandaid');
+$I->eyeball('bandaid');
+
+$I->amOnPage('/en/2011/10/26/overriding-drush-make');
+$I->eyeball('drush-make');
